ba82dfc5e34dbca9ecfef95a78170fa9ff4c138b
mingeon9411 <toube77@gmail.com>
2026-03-24 15:30:08 +0900

    Add files via upload

--- python/practice.py
+++ python/practice.py
@@ -3,3 +3,17 @@ print( 5 + 2 )
 print( 5 - 2 )
 print( 5 * 2 )
 print( 5 / 2 )
+
+print( 5 > 2 )
+print( 5 >= 2 )
+print( 5 < 2 )
+print( 5 <= 2 )
+print( 5 == 2 )
+print( 5 != 2 )
+
+print(3<5 and 7<5)
+print(3<5 or 7<5)
+print(not 3<5)
+
+print('c' in 'cat')
+print('c' not in 'cat')
